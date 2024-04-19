fb0ceab64442c6be1e7b346a961997bf21262b2a
Vinay Naikade <vinay.naikade@velotio.com>
2024-04-19 19:19:46 +0530

    Adding empty project

---
+++ ArticlesApp/ArticlesApp/ArticlesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticlesAppApp.swift
+//  ArticlesApp
+//
+//  Created by Vinay Naikade on 19/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ArticlesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
